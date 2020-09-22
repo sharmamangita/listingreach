e6d3d2a2f3a77a8cfae2381060887bc5e3aa156c
gurpreetsinghdev <gurpreetgamer@gmail.com>
2020-09-22 17:25:55 +0530

    ddd

--- frontend/webpack.config.prod.js
+++ frontend/webpack.config.prod.js
@@ -49,8 +49,8 @@ module.exports = {
     externals: {
         // global app config object
         config: JSON.stringify({
-            apiUrl: 'http://66.235.194.119:3006/api',
-            uploadapiUrl: 'http://66.235.194.119:3006',
+            apiUrl: 'http://66.235.194.119:3008/api',
+            uploadapiUrl: 'http://66.235.194.119:3008',
             assets: 'public/assets'
         })
     }
